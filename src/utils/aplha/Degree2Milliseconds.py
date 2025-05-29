6a162722b6f58951f76c35a3b07b9aa779cbb24b
marino <marino.alig@gmail.com>
2025-05-29 14:39:03 +0200

    Automatisierung

--- src/utils/aplha/Degree2Milliseconds.py
+++ src/utils/aplha/Degree2Milliseconds.py
@@ -1,10 +1,13 @@
-def drehung_in_ms(grad):
-    # Konstante: 180° entsprechen 2951 ms
-    ms_pro_grad = 2951 / 180
-    ms = grad * ms_pro_grad
-    return round(ms, 2)
+class Degree2Milliseconds:
+    def __init__(self):
+        # Konstante: 180° entsprechen 2951 ms
+        self.ms_pro_grad = 2951 / 180
+
+    def drehung_in_ms(self, grad):
+        ms = grad * self.ms_pro_grad
+        return round(ms, 2)
 
 # Beispielnutzung:
 if __name__ == "__main__":
-
-    print(drehung_in_ms(90))
+    drehung = Degree2Milliseconds()
+    print(drehung.drehung_in_ms(90))
