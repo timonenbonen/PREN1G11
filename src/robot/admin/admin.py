e43af71c8194fb98390f01111a1cfcc0b46a164c
timonenbonen <129446009+timonenbonen@users.noreply.github.com>
2025-06-05 18:05:26 +0200

    debug

--- src/robot/admin/admin.py
+++ src/robot/admin/admin.py
@@ -44,11 +44,11 @@ def process():
 
 def reset_tof():
     GPIO.setup(7, GPIO.OUT)
-    GPIO.output(7, GPIO.HIGH)
-    time.sleep(1)
     GPIO.output(7, GPIO.LOW)
     time.sleep(1)
     GPIO.output(7, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
     time.sleep(1)
 
 if __name__ == "__main__":
